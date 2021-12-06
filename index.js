d36fd070111286b077fef106322395a525262365
ahunter135 <austin.hunter@payzer.com>
2021-12-06 11:52:01 -0600

    add trivia

--- index.js
+++ index.js
@@ -375,7 +375,7 @@ app.listen(process.env.PORT || 5000, async () => {
 /**
  * Start IO Stuff
  */
-const io = new Server(http);
+const io = new Server({ app });
 
 io.on("connection", (socket) => {
   onlinePlayers++;
